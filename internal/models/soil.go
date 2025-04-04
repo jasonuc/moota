645fa698c63b8e6616fe24bff9cbaa7cfda85f3b
Jason <me@jasonuc.com>
2025-04-05 00:05:32 +0100

    feat: Add starter Soil model with area calculation and spatial methods

---
+++ internal/models/soil.go
@@ -0,0 +1,34 @@
+package models
+
+import "math"
+
+type SoilType int
+
+const (
+	TypeLoamy SoilType = iota
+	TypeSandy
+	TypeSilt
+	TypeClay
+)
+
+type Soil struct {
+	ID      string
+	Type    SoilType
+	Centre  Coordinates
+	RadiusM float64 // radius in metres
+}
+
+func (s *Soil) Area() float64 {
+	return math.Pi * (s.RadiusM * s.RadiusM)
+}
+
+func (s *Soil) ContainsPoint(p Coordinates) bool {
+	distance := s.Centre.Distance(p)
+	return distance <= s.RadiusM
+}
+
+func (s *Soil) OverlapsWith(other Soil) bool {
+	d := s.Centre.Distance(other.Centre)
+	// the two circles touching is included in this "overlap"
+	return d <= s.RadiusM+other.RadiusM
+}
