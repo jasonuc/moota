2e6a4cfec21404affb48a3aa19f620d8ea99e4f0
Jason <me@jasonuc.com>
2025-04-06 23:14:05 +0100

    Add new fields to Plant and new Alive method; Add (incomplete) Plant constructor

--- internal/models/plant.go
+++ internal/models/plant.go
@@ -1,41 +1,63 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+const PLANT_INTERACTION_RADIUSM = 2.0 // TODO: This value is up for debate
 
 type Plant struct {
 	ID            string
 	Nickname      string
-	centre        Coordinates
-	radiusM       float64 // interaction radius
 	Health        float64
+	Xp            int64
+	Level         int64
+	Dead          bool
+	Soil          *Soil
+	Tempers       *Tempers
 	PlantedAt     time.Time
 	LastWateredAt time.Time
-	Xp            int64
-	Soil          *Soil
-	Tempers       Tempers
 	SeedMeta
+	CircleMeta
 }
 
-func (p *Plant) Centre() Coordinates {
-	return p.centre
+func NewPlant(seed *Seed, soil *Soil, centre Coordinates) (*Plant, error) {
+	nickname := "Atura" // TODO: Make a function to generate random whimsical names
+	seed.Planted = true
+	circleMeta := CircleMeta{radiusM: PLANT_INTERACTION_RADIUSM, centre: centre}
+	if !IsInsideSoil(circleMeta, soil) {
+		return nil, fmt.Errorf("plant is not completely inside soil")
+	}
+
+	healthBonus := 0.0
+	if seed.SeedMeta.OptimalSoil == soil.Type {
+		healthBonus += 1.5
+	} else {
+		if seed.SeedMeta.IsCompatibleWithSoil(soil.Type) {
+			healthBonus += 0.5
+		} else {
+			healthBonus -= 0.5
+		}
+	}
+
+	// TODO: Compe up with an Xp and Levelling system
+	// TODO: Work on giving plants bonus xp when planted on optimal soil
+
+	return &Plant{
+		Nickname:  nickname,
+		Health:    seed.Health + healthBonus,
+		Soil:      soil,
+		Tempers:   NewTempers(),
+		PlantedAt: time.Now(),
+		SeedMeta:  seed.SeedMeta,
+		CircleMeta: CircleMeta{
+			centre:  centre,
+			radiusM: PLANT_INTERACTION_RADIUSM,
+		},
+	}, nil
 }
 
-func (p *Plant) RadiusM() float64 {
-	return p.radiusM
+func (p *Plant) Alive() bool {
+	return !p.Dead
 }
-
-func (p *Plant) OverlapsWith(other Circle) bool {
-	d := p.centre.DistanceM(other.Centre())
-	return d <= p.RadiusM()+other.RadiusM()
-}
-
-func (p *Plant) ContainsPoint(other Coordinates) bool {
-	distance := p.centre.DistanceM(other)
-	return distance <= p.RadiusM()
-}
-
-func (p *Plant) IsInside(other Circle) bool {
-	d := p.Centre().DistanceM(other.Centre())
-	return d+p.RadiusM() <= other.RadiusM()
-}
-
