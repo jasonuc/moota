7d8bb789644eb7d7b6dbf291d359973c2b286182
Jason <me@jasonuc.com>
2025-04-06 22:29:49 +0100

    feat: Introduce CircleMeta struct and remove IsInside method from circle

--- internal/models/circle.go
+++ internal/models/circle.go
@@ -3,7 +3,28 @@ package models
 type Circle interface {
 	Centre() Coordinates
 	RadiusM() float64 // radius in metres
-	OverlapsWith(Circle) bool
 	ContainsPoint(Coordinates) bool
-	IsInside(Circle) bool
+	OverlapsWith(Circle) bool
+}
+
+type CircleMeta struct {
+	centre  Coordinates
+	radiusM float64
+}
+
+func (c CircleMeta) Centre() Coordinates {
+	return c.centre
+}
+
+func (c CircleMeta) RadiusM() float64 {
+	return c.radiusM
+}
+
+func (c CircleMeta) ContainsPoint(p Coordinates) bool {
+	return c.centre.DistanceM(p) <= c.RadiusM()
+}
+
+func (c CircleMeta) OverlapsWith(other Circle) bool {
+	d := c.centre.DistanceM(other.Centre())
+	return d <= c.RadiusM()+other.RadiusM()
 }
