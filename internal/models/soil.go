284d879c3efb984a905f1ca43e6ccab69e0906b5
Jason <me@jasonuc.com>
2025-04-07 18:11:21 +0100

    feat: Add new SoilRadius values; Add new constructors for soil defaulting to new radius values; Update the Soil constructor works

--- internal/models/soil.go
+++ internal/models/soil.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"math"
 	"math/rand/v2"
 	"time"
 )
@@ -14,26 +15,72 @@ const (
 	SoilTypeClay
 )
 
-type Soil struct {
-	ID             string
-	Type           SoilType
+type SoilMeta struct {
+	Type SoilType
+	// Min: 0.00; Max 5.00
 	WaterRetention float64
 	Nutrients      float64
-	CreatedAt      time.Time
+}
+
+var (
+	SoilMetaLoam = SoilMeta{
+		Type:           SoilTypeLoam,
+		WaterRetention: 3.50,
+		Nutrients:      4.00,
+	}
+	SoilMetaSandy = SoilMeta{
+		Type:           SoilTypeSandy,
+		WaterRetention: 1.50,
+		Nutrients:      2.00,
+	}
+	SoilMetaSilt = SoilMeta{
+		Type:           SoilTypeSilt,
+		WaterRetention: 3.00,
+		Nutrients:      3.50,
+	}
+	SoilMetaClay = SoilMeta{
+		Type:           SoilTypeClay,
+		WaterRetention: 4.50,
+		Nutrients:      2.50,
+	}
+)
+
+type Soil struct {
+	ID        string
+	CreatedAt time.Time
+	SoilMeta
 	CircleMeta
 }
 
-// TODO: Make Soild.radiusM definitive values like, Small, Medium and Large
+const (
+	SoilRadiusMSmall  = 8.92  // For ≈250 sq. meters
+	SoilRadiusMMedium = 17.84 // For ≈1,000 sq. meters
+	SoilRadiusMLarge  = 30.90 // For ≈3,000 sq. meters
+)
 
-func NewSoil(soilType SoilType, centre Coordinates, radiusM float64, createdAt time.Time) *Soil {
+func NewSmallSizedSoil(soilMeta SoilMeta, centre Coordinates, createdAt time.Time) *Soil {
+	return newSoil(soilMeta, centre, createdAt, SoilRadiusMSmall)
+}
+
+func NewMediumSizedSoil(soilMeta SoilMeta, centre Coordinates, createdAt time.Time) *Soil {
+	return newSoil(soilMeta, centre, createdAt, SoilRadiusMMedium)
+}
+
+func NewLargeSizedSoil(soilMeta SoilMeta, centre Coordinates, createdAt time.Time) *Soil {
+	return newSoil(soilMeta, centre, createdAt, SoilRadiusMLarge)
+}
+
+func newSoil(soilMeta SoilMeta, centre Coordinates, createdAt time.Time, radiusM float64) *Soil {
+	// ±3 Offset
+	soilMeta.Nutrients += math.Round((rand.Float64()-0.5)*0.6*100) / 100
+	soilMeta.WaterRetention += math.Round((rand.Float64()-0.5)*0.6*100) / 100
+
 	return &Soil{
-		Type: soilType,
 		CircleMeta: CircleMeta{
 			centre:  centre,
 			radiusM: radiusM,
 		},
-		WaterRetention: rand.Float64() * 5,
-		Nutrients:      rand.Float64() * 5,
-		CreatedAt:      createdAt,
+		CreatedAt: createdAt,
+		SoilMeta:  soilMeta,
 	}
 }
