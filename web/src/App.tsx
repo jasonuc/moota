ca872bc3b12548240c3e6dfbe95d55b983ef5374
Jason <me@jasonuc.com>
2025-04-18 10:52:31 +0100

    feat: add tailwindcss config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div>
-      Moota
+    <div className="font-archivo bg-background h-screen w-screen flex items-center justify-center">
+      <h1 className="font-extrabold text-7xl">Moota</h1>
     </div>
   )
 }
