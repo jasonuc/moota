58fdb775b1ba7da5186df72c9227545f44260ec3
Jason <me@jasonuc.com>
2025-04-25 00:45:57 +0100

    feat: add 'on delete' actions to seeds and plants table

--- migrations/000002_create_seeds_table.up.sql
+++ migrations/000002_create_seeds_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS seeds (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-	owner_id UUID REFERENCES users (id),
+	owner_id UUID REFERENCES users (id) ON DELETE CASCADE,
 	hp REAL NOT NULL,
 	planted BOOLEAN NOT NULL DEFAULT FALSE,
 	optimal_soil VARCHAR(30) NOT NULL,
