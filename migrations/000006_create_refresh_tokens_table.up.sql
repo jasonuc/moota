37647f8a0302db932159a08ccdb46b0d1e0a0d84
Jason <me@jasonuc.com>
2025-04-25 01:07:55 +0100

    feat: remove default 'created at' time for refresh token as always it provided

--- migrations/000006_create_refresh_tokens_table.up.sql
+++ migrations/000006_create_refresh_tokens_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     hash BYTEA NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMPTZ NOT NULL,
     expires_at TIMESTAMPTZ NOT NULL,
     revoked_at TIMESTAMPTZ
 );
