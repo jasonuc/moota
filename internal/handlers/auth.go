d20e6e7da93f09ef28d490903a49da37d60ef97a
Jason <me@jasonuc.com>
2025-04-25 22:15:40 +0100

    feat: add new auth handler struct and empty utils file in handlers package

---
+++ internal/handlers/auth.go
@@ -0,0 +1,13 @@
+package handlers
+
+import "github.com/jasonuc/moota/internal/services"
+
+type AuthHandler struct {
+	authService *services.AuthService
+}
+
+func NewAuthHandler(authService *services.AuthService) *AuthHandler {
+	return &AuthHandler{
+		authService: authService,
+	}
+}
