fc413bcb9a111095cb0f184f3afde2323f4ef49e
Jason <me@jasonuc.com>
2025-05-02 04:37:28 +0100

    feat: add new auth handlers and refactor routes to use new ValidateUserAccess middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,24 +20,37 @@ func (app *application) routes() http.Handler {
 			r.Post("/register", app.authHandler.HandleRegisterRequest)
 			r.Post("/login", app.authHandler.HandleLoginRequest)
 			r.Post("/refresh", app.authHandler.HandleTokenRefresh)
+
+			r.Route("/u/{userID}", func(r chi.Router) {
+				r.Use(app.authMiddleware.ValidateUserAccess)
+
+				r.Patch("/change-email", app.authHandler.HandleChangeEmail)
+				r.Patch("/change-password", app.authHandler.HandleChangePassword)
+				r.Patch("/change-username", app.authHandler.HandleChangeUsername)
+			})
 		})
 
 		r.Group(func(r chi.Router) {
 			r.Use(app.authMiddleware.Authorise)
 
 			r.Route("/plants", func(r chi.Router) {
-				r.Get("/u/{userID}", app.plantHandler.HandleGetAllUserPlants)
-				r.Get("/u/{userID}/graveyard", app.plantHandler.HandleGetAllUserDeceasedPlants)
+				r.Route("/u/{userID}", func(r chi.Router) {
+					r.Use(app.authMiddleware.ValidateUserAccess)
+
+					r.Get("/", app.plantHandler.HandleGetAllUserPlants)
+					r.Get("/graveyard", app.plantHandler.HandleGetAllUserDeceasedPlants)
+				})
 
 				r.Get("/{plantID}", app.plantHandler.HandleGetPlant)
-
 				r.Post("/{plantID}/action", app.plantHandler.HandleActionOnPlant)
 				r.Post("/{plantID}/activate", app.plantHandler.HandleActivatePlant)
 				r.Post("/{plantID}/kill", app.plantHandler.HandleKillPlant)
 			})
 
 			r.Route("/seeds", func(r chi.Router) {
-				r.Get("/u/{userID}", app.seedHandler.HandleGetUserSeeds)
+				r.Route("/u/{userID}", func(r chi.Router) {
+					r.Get("/", app.seedHandler.HandleGetUserSeeds)
+				})
 
 				r.Post("/{seedID}", app.seedHandler.HandlePlantSeed)
 			})
