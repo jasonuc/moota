46d694a75c72b949fbd9047afd8499dcc591073d
Jason <me@jasonuc.com>
2025-06-01 13:00:59 +0100

    chore: udpate deps and add vite config; install axios

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,29 +1,31 @@
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    allowedHosts: [".ngrok-free.app"],
-    proxy: {
-      "/api": {
-        target: process.env.API_URL || "http://localhost:8080",
-        changeOrigin: true,
-        secure: process.env.NODE_ENV === "production",
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL || "http://localhost:8080",
+          changeOrigin: true,
+          secure: process.env.VITE_API_URL ? true : false,
+        },
       },
     },
-  },
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+      TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
