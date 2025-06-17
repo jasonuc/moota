568ab12fec534f62844cd49fa8c4fe6ed5752eba
Jason <me@jasonuc.com>
2025-06-17 01:46:48 +0100

    feat: add very basic cors setup for development

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,10 +6,24 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{
+			"https://moota.localhost",
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
+		},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Content-Type"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	r.Use(middleware.Logger)
 
