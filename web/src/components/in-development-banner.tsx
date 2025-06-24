02ccec24d91598ed3dd7ef00b253a6385e05b895
Jason <me@jasonuc.com>
2025-06-24 04:08:03 +0300

    feat: add an in development banner component which is part of the root component above the App component

---
+++ web/src/components/in-development-banner.tsx
@@ -0,0 +1,32 @@
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { AlertCircleIcon, XIcon } from "lucide-react";
+
+export default function InDevelopmentBanner() {
+  const [showInDevelopmentBanner, setShowInDevelopmentBanner] = useLocalStorage(
+    "show-in-development-banner",
+    true
+  );
+
+  if (!showInDevelopmentBanner) return null;
+
+  return (
+    <div className="p-2 flex items-center justify-between gap-x-1.5 bg-amber-600/70">
+      <div className="flex items-center gap-x-1.5">
+        <AlertCircleIcon className="size-3" />
+        <small className="font-semibold">
+          Currently in development. Breaking changes could occur.
+        </small>
+      </div>
+
+      <button
+        onClick={() => {
+          setShowInDevelopmentBanner(false);
+        }}
+      >
+        <div>
+          <XIcon className="size-4" />
+        </div>
+      </button>
+    </div>
+  );
+}
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,14 @@
 import App from "@/app";
 import { Toaster } from "@/components/ui/sonner";
 import AuthProvider from "@/services/providers/auth-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import InDevelopmentBanner from "./components/in-development-banner";
 import "./index.css";
 import GeolocationProvider from "./services/providers/geolocation-provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <GeolocationProvider>
+            <InDevelopmentBanner />
             <App />
             <Toaster />
           </GeolocationProvider>
