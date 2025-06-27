f7c6a0a6f2d992e9580ea0ea0588f51ebe2f5403
Jason <me@jasonuc.com>
2025-06-27 15:42:09 +0100

    feat: add refresh access token middlware to axios instance

--- web/src/services/api/index.ts
+++ web/src/services/api/index.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 const ax = axios.create({
   baseURL:
@@ -6,5 +6,39 @@ const ax = axios.create({
   validateStatus: (status) => status < 300,
   withCredentials: true,
 });
+
+ax.interceptors.response.use(
+  (response) => response,
+  refreshAccessTokenMiddleware
+);
+
+interface AxiosRequestConfigExtended extends AxiosRequestConfig {
+  _retry?: boolean;
+}
+
+async function refreshAccessTokenMiddleware(error: AxiosError) {
+  const originalRequest = error.config as AxiosRequestConfigExtended;
+
+  const isAuthRoute = originalRequest?.url?.includes("/auth/");
+
+  if (
+    error.response?.status !== 401 ||
+    originalRequest?._retry ||
+    isAuthRoute ||
+    !originalRequest?.url
+  ) {
+    return Promise.reject(error);
+  }
+
+  originalRequest._retry = true;
+
+  try {
+    await ax.post("/auth/refresh");
+    return ax(originalRequest);
+  } catch (refreshError) {
+    window.dispatchEvent(new CustomEvent("auth:logout"));
+    return Promise.reject(refreshError);
+  }
+}
 
 export { ax };
