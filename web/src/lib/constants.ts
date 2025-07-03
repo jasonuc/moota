84c46e11ccb37952f2af171dccc23026ae5c15b7
tkahng <tkahng@gmail.com>
2025-07-02 19:31:27 -0700

    map lib

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const EARTH_RADIUS_M = 6.378e6;
-export const GEOLOCATION_DISTANCE_ACCURACY_ALLOWANCE = 20; // TODO: This value is still experimental
+export const GEOLOCATION_DISTANCE_ACCURACY_ALLOWANCE = 100; // TODO: This value is still experimental
 export const PLANT_INTERACTION_RADIUS = 20; // TODO: This value is still experimental
