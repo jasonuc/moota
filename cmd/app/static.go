e90f761b58f7356f7617aa3c21af9f2c701ac1a5
Jason <me@jasonuc.com>
2025-11-14 07:45:49 +0000

    feat: use embedded filesystem for accessing static files

--- cmd/app/static.go
+++ cmd/app/static.go
@@ -1,26 +1,27 @@
 package main
 
 import (
+	"io/fs"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/jasonuc/moota/internal/utils"
+	"github.com/jasonuc/moota/web"
 )
 
 func (app *application) serveStaticFiles(r chi.Router) {
 	staticPath := app.cfg.static.path
 
-	if _, err := os.Stat(staticPath); os.IsNotExist(err) {
-		app.logger.Panicf("static files directory not found: %s", staticPath)
+	staticFs, err := fs.Sub(web.Files, staticPath)
+	if err != nil {
+		app.logger.Panicf("%s does not exist on the embedded file system: %v", staticPath, err)
 	}
 
-	r.Handle("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir(filepath.Join(staticPath, "assets")))))
+	r.Handle("/assets/*", http.FileServerFS(staticFs))
 
-	r.Handle("/favicon.ico", http.FileServer(http.Dir(staticPath)))
-	r.Handle("/moota.png", http.FileServer(http.Dir(staticPath)))
+	r.Handle("/favicon.ico", http.FileServerFS(staticFs))
+	r.Handle("/moota.png", http.FileServerFS(staticFs))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		if strings.HasPrefix(r.URL.Path, "/api/") {
@@ -28,7 +29,6 @@ func (app *application) serveStaticFiles(r chi.Router) {
 			return
 		}
 
-		indexPath := filepath.Join(staticPath, "index.html")
-		http.ServeFile(w, r, indexPath)
+		http.ServeFileFS(w, r, staticFs, "index.html")
 	})
 }
